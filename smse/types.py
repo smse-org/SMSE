59fdda0758dc1380c097a2e7c5a2dc32a6823bae
Ahmedsaed <ahmedsaed2652003@gmail.com>
2025-02-27 00:11:06 +0200

    fix: add Modality enum and Embedding Type

--- smse/types.py
+++ smse/types.py
@@ -1,13 +1,27 @@
 from dataclasses import dataclass
+from enum import Enum, auto
 from typing import List
 
 import numpy as np
 import torch
+from numpy import ndarray
 from numpy.typing import NDArray
 from PIL import Image
+from torch import Tensor
+
+
+class Modality(Enum):
+    """Supported modalities in the SMSE framework."""
+
+    TEXT = auto()
+    IMAGE = auto()
+    AUDIO = auto()
+    VIDEO = auto()
+
 
 ImageT = NDArray[np.float64 | np.uint8] | Image.Image | torch.Tensor
 TextT = str
+EmbeddingT = List[Tensor] | ndarray | Tensor
 
 
 @dataclass
